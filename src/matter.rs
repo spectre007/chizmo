3fa386a14cef9c5e7c4a79f989805ec1d2e52629
Alexander Zech <alexzech@gmx.de>
2023-09-10 22:25:28 -0700

    chore: add fn to compute distance matrix

--- src/matter.rs
+++ src/matter.rs
@@ -1,3 +1,4 @@
+use na::base::{DMatrix, Vector3};
 
 pub struct Atom {
     pub element: String,
@@ -5,3 +6,22 @@ pub struct Atom {
     pub y: f64,
     pub z: f64,
 }
+
+fn compute_distance_matrix(atoms: &Vec<Atom>) -> DMatrix<f64> {
+    let n_atoms = atoms.len();
+    let mut dist = DMatrix::zeros(n_atoms, n_atoms);
+
+    for i in 0..n_atoms {
+        let a = Vector3::new(atoms[i].x, atoms[i].y, atoms[i].z);
+        for j in 0..i {
+            if i == j { continue; }
+            let b = Vector3::new(atoms[j].x, atoms[j].y, atoms[j].z);
+            let norm = (b-a).norm();
+            dist[(i, j)] = norm;
+            dist[(j, i)] = norm;
+        }
+    }
+
+    dist
+}
+

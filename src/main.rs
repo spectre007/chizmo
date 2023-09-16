e59ebe8c42cf8fef69f677f3054b031662ec93e8
Alexander Zech <alexzech@gmx.de>
2023-09-16 00:43:06 -0700

    refactor: move main to cli tool

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-extern crate nalgebra as na;
-
-use chizmo::constants::get_element;
-use chizmo::matter::get_fragments;
-use chizmo::matter::Atom;
-use chizmo::parse_xyz;
+use clap::Parser;
 use std::fs;
+use chizmo::parse_xyz;
+use chizmo::matter::Atom;
+use chizmo::matter::get_fragments;
+
+#[derive(Parser)]
+struct Cli {
+    // path: std::path::PathBuf,
+    path: String,
+}
 
 fn main() {
-    const FNAME: &str = "test_case.xyz";
-    let content = fs::read_to_string(&FNAME).expect("Failed to read file!");
-
+    let args = Cli::parse();
+    let content = fs::read_to_string(args.path).expect("Failed to read file!");
     let atoms: Vec<Atom> = parse_xyz(&content).unwrap();
+    let fragments: Vec<Vec<Atom>> = get_fragments(&atoms);
 
-    println!("Read {} atoms", atoms.len());
-
-    let hydrogen = get_element("H").unwrap();
-
-    println!("H name {}", hydrogen.name);
-
-    let indices = get_fragments(&atoms);
-    for frag in indices {
-        println!("-------");
+    for frag in fragments {
+        println!("---");
         for atom in frag {
             println!("{:<2} {: >7.3} {: >7.3} {: >7.3}", atom.element, atom.x, atom.y, atom.z);
         }
